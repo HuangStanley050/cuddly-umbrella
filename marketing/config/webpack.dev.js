618df421cab61dfa13559074c28f8e3e1e858a14
Stanley Huang <infamous_godhand@yahoo.com>
2021-04-20 10:55:25 +1000

    start marketing app

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,4 +1,4 @@
-const { merget } = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
 
