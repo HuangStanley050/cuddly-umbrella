f7eb1ed9883a4fa5f8085b19221c59e2b5d87715
Stanley Huang <infamous_godhand@yahoo.com>
2021-04-26 09:16:25 +1000

    display contents in marketing app

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 export const mount = (htmlElement) => {
-  ReactDOM.render(<h1>hello</h1>, htmlElement);
+  ReactDOM.render(<App />, htmlElement);
 };
 
 if (process.env.NODE_ENV === "development") {
